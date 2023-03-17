8a3cd4fa97e25c6006f3c2d36365cab67397b7e3
Sai Kumar Yamarthi <saikumar.y786@gmail.com>
2023-03-17 20:58:40 +0530

    js

---
+++ password.js
@@ -0,0 +1,14 @@
+let password=document.getElementById("fakepassword");
+let toggler=document.getElementById("toggler");
+showHidePassword = () => {
+    if(password.type == "password") 
+    {
+        password.setAttribute("type","text");
+        toggler.classList.add("fa-eye-slash");
+    }
+    else{
+        toggler.classList.remove("fa-eye-slash");
+        password.setAttribute("type","password");
+    }
+};
+toggler.addEventListener("click",showHidePassword);
